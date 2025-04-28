ab68288a907fc37f3aa98524787c191ec8859a60
Rafael Stamatie <stamatieionrafael4@gmail.com>
2025-04-29 00:31:00 +0300

    Refactor script to improve prefomance

--- setup-docker-vms.sh
+++ setup-docker-vms.sh
@@ -1,57 +1,63 @@
 #!bin/bash
 
 
- echo "Ce vrei sa configurezi?"
- echo "1) VM1 - mysql"
- echo "2) VM2 - wordpress"
- read -p "Alege optiunea (1 sau 2): " optiune
+ echo "What do you want to configure?"
+ echo "1) Mysql"
+ echo "2) Wordpress"
+ read option
 
- if [ "$optiune" == "1" ]; then
-     echo "Configurare VM1 - mysql"
+ if [ "$option" = "1" ]; then
+     echo "Setting up Mysql"
 
-     sudo apt update
+     sudo apt update && sudo apt upgrade -y
      sudo apt install -y docker.io
 
      sudo systemctl start docker
      sudo systemctl enable docker
 
-    ip_vm1=$(hostname -I | awk '{print $1}')
-    echo "Ip-ul detectat automat pe VM1 este :$ip_vm1"
+    ip_vm_mysql=$(hostname -I | awk '{print $1}')
 
     docker run -d --name mysql-container \
       -e MYSQL_ROOT_PASSWORD=rtpass \
       -e MYSQL_DATABASE=wordpress \
       -e MYSQL_USER=wpuser \
-      -e MYSQL_PASSWORD=parola \
+      -e MYSQL_PASSWORD=password \
       -p 4000:3306 \
       mysql:5.7
 
-   echo "Mysql ruleaza pe ip-ul $ip_vm1:4000"
+   echo "Mysql running at $ip_vm_mysql:4000"
 
- elif [ "$optiune" == "2" ]; then
-     echo "Configurare VM2 - Wordpress"
+ elif [ "$option" = "2" ]; then
+     echo "Setting up VM - Wordpress"
+     
+     read -p "Enter the Mysql-Vm-Ip): " ip_vm_mysql
+     if [ -z "$ip_vm_mysql" ];then
+	     echo "ERROR: You must specify the mysql_vm_ip."
+	 exit 1
+     fi
 
-     sudo apt update 
+     sudo apt update && sudo apt upgrade -y
      sudo apt install -y docker.io lynx
 
      sudo systemctl start docker 
      sudo systemctl enable docker 
 
-     ip_vm2=$(hostname -I | awk '{print $1}')
-     echo "Ip-ul detectat automat pe VM2 este : $ip_vm2"
-
-     read -p "Introduceti ip-ul vm1: " ip_vm1
+     ip_vm_wp=$(hostname -I | awk '{print $1}')
+    
+     docker pull wordpress:6.8-php8.1-apache
 
      docker run -d --name wordpress-container \
-       -e WORDPRESS_DB_HOST=${ip_vm1}:4000 \
+       -e WORDPRESS_DB_HOST=${ip_vm_mysql}:4000 \
        -e WORDPRESS_DB_USER=wpuser \
-       -e WORDPRESS_DB_PASSWORD=parola \
+       -e WORDPRESS_DB_PASSWORD=password \
        -e WORDPRESS_DB_NAME=wordpress \
        -p 6080:80
-      wordpress:latest
+      wordpress:6.8-php8.1-apache
+     
+    echo "Wordpress is running $ip_vm_wp:6080 and connected to Mysql $ip_vm_mysql:4000"
 
-      lynx http://ip_vm2:6080
+     # lynx http://$ip_vm_wp:6080
 
  else 
-    echo " Optiune invalida.Alege 1 sau 2"
+    echo "Invalid option.Please choose 1 or 2!"
  fi
